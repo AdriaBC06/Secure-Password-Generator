23717b9ce83162078e598b7359b587c9f67d7020
Adrià <adriabc06@gmail.com>
2025-05-22 21:17:40 +0200

    Script generate passwords

---
+++ script.js
@@ -0,0 +1,48 @@
+const MAYUS_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const MINUS_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+const NUMBERS = "1234567890";
+const SPECIAL_CHARACTERS = ".!?;:'@#$%^&*_-+=~`|/(){}[]<>";
+
+let applyMayus = false;
+let applyMinus = false;
+let applyNumbers = false;
+let applySpecial = false;
+
+let passwordPosibilities = "";
+
+let passwordLength = 5;
+
+let password = "";
+
+function generatePassword() {
+
+    if (applyMayus) {
+        passwordPosibilities += MAYUS_LETTERS;
+    }
+
+    if (applyMinus) {
+        passwordPosibilities += MINUS_LETTERS;
+    }
+
+    if (applyNumbers) {
+        passwordPosibilities += NUMBERS;
+    }
+
+    if (applySpecial) {
+        passwordPosibilities += SPECIAL_CHARACTERS;
+    }
+
+    console.log(passwordPosibilities);
+    console.log(passwordPosibilities.length);
+    console.log(passwordLength);
+
+    if (passwordPosibilities.length > 0 && passwordLength > 0) {
+        for (let i = 0; i < passwordLength; i++) {
+            password += passwordPosibilities.charAt(Math.floor(Math.random() * passwordPosibilities.length));
+        }
+    }
+
+    document.getElementById("password").innerHTML = password;
+}
+
+generatePassword();
