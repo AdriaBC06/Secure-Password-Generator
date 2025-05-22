e35d44f720631c1cb73f7554cdb2179f34a0b67f
Adrià <adriabc06@gmail.com>
2025-05-22 22:45:12 +0200

    Local Storage, Apply characters in password

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const MINUS_LETTERS = "abcdefghijklmnopqrstuvwxyz";
 const NUMBERS = "1234567890";
 const SPECIAL_CHARACTERS = ".!?;:'@#$%^&*_-+=~`|/(){}[]<>";
 
-let applyMayus = false;
-let applyMinus = false;
-let applyNumbers = false;
-let applySpecial = false;
+let applyMayus;
+let applyMinus;
+let applyNumbers;
+let applySpecial;
 
 let passwordPosibilities = "";
 
@@ -14,30 +14,45 @@ let passwordLength = 5;
 
 let password = "";
 
+console.log(localStorage.getItem("applyMayus"));
+
+document.getElementById("mayus").checked = localStorage.getItem("applyMayus") === "true";
+document.getElementById("minus").checked = localStorage.getItem("applyMinus") === "true";
+document.getElementById("numbers").checked = localStorage.getItem("applyNumbers") === "true";
+document.getElementById("special").checked = localStorage.getItem("applySpecial") === "true";
+
 function generatePassword() {
     password = "";
 
     passwordPosibilities = "";
 
-    if (applyMayus) {
+    if (document.getElementById("mayus").checked) {
         passwordPosibilities += MAYUS_LETTERS;
+        applyMayus = true;
+    } else {
+        applyMayus = false;
     }
 
-    if (applyMinus) {
+    if (document.getElementById("minus").checked) {
         passwordPosibilities += MINUS_LETTERS;
+        applyMinus = true;
+    } else {
+        applyMinus = false;
     }
 
-    if (applyNumbers) {
+    if (document.getElementById("numbers").checked) {
         passwordPosibilities += NUMBERS;
+        applyNumbers = true;
+    } else {
+        applyNumbers = false;
     }
 
-    if (applySpecial) {
+    if (document.getElementById("special").checked) {
         passwordPosibilities += SPECIAL_CHARACTERS;
+        applySpecial = true;
+    } else {
+        applySpecial = false;
     }
-
-    console.log(passwordPosibilities);
-    console.log(passwordPosibilities.length);
-    console.log(passwordLength);
 
     if (passwordPosibilities.length > 0 && passwordLength > 0) {
         for (let i = 0; i < passwordLength; i++) {
@@ -51,3 +66,19 @@ function generatePassword() {
 generatePassword();
 
 document.getElementById("generatePasswordButton").addEventListener("click", generatePassword);
+
+document.getElementById("mayus").addEventListener("change", () => 
+    {localStorage.setItem("applyMayus", 
+        document.getElementById("mayus").checked)});
+
+document.getElementById("minus").addEventListener("change", () => 
+    {localStorage.setItem("applyMinus", 
+        document.getElementById("minus").checked)});
+
+document.getElementById("numbers").addEventListener("change", () => 
+    {localStorage.setItem("applyNumbers", 
+        document.getElementById("numbers").checked)});
+
+document.getElementById("special").addEventListener("change", () => 
+    {localStorage.setItem("applySpecial", 
+        document.getElementById("special").checked)});
